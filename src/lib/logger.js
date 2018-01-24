e6a89be2c985381eb5cf6407f78f091a127c9955
Sudheer Kumar Tumarada <stumarada@sapient.com>
2018-01-24 18:36:35 +0530

    updated log warnings

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -8,20 +8,20 @@ const config = {
 
 const log = (msg) => {
   if (config.isDebugMode) {
-    console.log(chalk.green(`\nlog: ${config.prefix} ${msg}`));
+    console.log(chalk.green(`\nlog: ${config.prefix} ${msg}\n`));
   }
 };
 
 const info = (msg) => {
-  console.info(chalk.green(`\ninfo: ${config.prefix} ${msg}`));
+  console.info(chalk.green(`\ninfo: ${config.prefix} ${msg}\n`));
 };
 
 const warn = (msg) => {
-  console.warn(chalk.blue(`\nwarning: ${config.prefix} ${msg}`));
+  console.warn(chalk.blue(`\nwarning: ${config.prefix} ${msg}\n`));
 };
 
 const error = (msg) => {
-  console.error(chalk.red(`\nerror: ${config.prefix} ${msg}`));
+  console.error(chalk.red(`\nerror: ${config.prefix} ${msg}\n`));
 };
 module.exports = {
   log,
