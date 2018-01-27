6fe4996de5d513d991f5e91273256d43db4973fb
tsudheerkumar <tsudheerkumar@users.noreply.github.com>
2018-01-27 12:58:37 +0530

    Feature/loggerpath (#3)
    * Resolved logger path issue
    
    * Readme updated and bumped version

--- test/index.js
+++ test/index.js
@@ -4,9 +4,11 @@ GitHaki([
   {
     name: 'pre-commit',
     command: 'npm test',
+    bashDest: '.git/hooks',
   },
   {
     name: 'pre-push',
     command: 'npm test',
+    bashDest: '.git/hooks',
   },
 ]);
